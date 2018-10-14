cb73945700606274bc17ad369cee0124762e5799
mathematicalanalysis <32415898+mathematicalanalysis@users.noreply.github.com>
2018-10-14 15:32:47 +0800

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i; j < nums.size(); j++)
+            {
+                if(nums[i] + nums[j] == target && i != j)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
